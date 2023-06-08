5adfde8814fd8282571b78711db225bb7628f9d3
Andreas McDermott <andreas.mcdermott@shortcut.com>
2023-06-07 18:40:27 -0700

    Simplify + add movement.

--- game.mjs
+++ game.mjs
@@ -4,8 +4,13 @@ let mapKeyName = (key) => (key === " " ? "Space" : key);
 let ctx2d;
 let w, h;
 let lt;
-let asteroid_initial_width = [1, 1.5];
-let assets = {};
+let asteroid_levels = 3;
+let asteroid_widths = [
+  [72, 112],
+  [48, 64],
+  [16, 32],
+];
+let asteroid_speed = 2;
 let input = {};
 let player;
 let asteroids;
@@ -23,51 +28,47 @@ export function init(canvas) {
     delete input[mapKeyName(e.key)];
   });
 
-  loadAssets(() => {
-    initLevel();
-    lt = performance.now();
-    requestAnimationFrame(nextFrame);
-  });
-}
-
-async function loadAssets(onDone) {
-  await Promise.all([
-    loadImgAsset("spaceship"),
-    loadImgAsset("ufo"),
-    loadImgAsset("asteroid_0"),
-    loadImgAsset("asteroid_1"),
-  ]);
-  onDone();
+  initLevel();
+  lt = performance.now();
+  requestAnimationFrame(nextFrame);
 }
 
 function initLevel() {
   player = {
-    sprite: assets.spaceship,
-    r: assets.spaceship.width / 2,
-    scale: 0.5,
+    w: 24,
+    h: 64,
     x: w / 2,
     y: h / 2,
-    angle: 90,
+    angle: 270,
+    rot: 0,
   };
   asteroids = [];
+  let lvl = 0;
   for (let i = 0; i < 3; ++i) {
-    let sprite = [assets.asteroid_0, assets.asteroid_1][
-      Math.floor(Math.random() * 2)
-    ];
-    let scale =
-      Math.random() * (asteroid_initial_width[1] - asteroid_initial_width[0]) +
-      asteroid_initial_width[0];
-    let r = sprite.width * 0.5;
-    console.log(scale, r, sprite.width);
     asteroids.push({
-      sprite,
-      r,
-      scale,
+      level: lvl,
+      r:
+        Math.random() * (asteroid_widths[lvl][1] - asteroid_widths[lvl][0]) +
+        asteroid_widths[lvl][0],
       x: Math.random() * w,
       y: Math.random() * h,
       angle: Math.random() * 360,
-      rot: Math.random() * 0.5 - 0.25,
+      rot: Math.random() * 0.2 - 0.1,
+      v: new Vec2(Math.random() * 2 - 1, Math.random() * 2 - 1).normalize(),
     });
+  }
+}
+
+class Vec2 {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  normalize() {
+    let len = Math.sqrt(this.x * this.x + this.y * this.y);
+    this.x = this.x / len;
+    this.y = this.y / len;
+    return this;
   }
 }
 
@@ -82,33 +83,69 @@ function gameLoop(dt) {
   ctx2d.clearRect(0, 0, w, h);
 
   // updatePlayer();
-  draw(player);
+  drawPlayer(player);
 
   for (let i = 0; i < asteroids.length; ++i) {
-    update(dt, asteroids[i]);
-    draw(asteroids[i]);
+    updateAsteroid(dt, asteroids[i]);
+    drawAsteroid(asteroids[i]);
   }
 }
 
-function update(dt, entity) {
-  entity.angle += entity.rot * dt;
-  if (entity.angle >= 360) entity.angle -= 360;
+function updateAsteroid(dt, asteroid) {
+  asteroid.angle += asteroid.rot * dt;
+  if (asteroid.angle >= 360) asteroid.angle -= 360;
+  asteroid.x += asteroid.v.x * asteroid_speed;
+  asteroid.y += asteroid.v.y * asteroid_speed;
+  if (asteroid.x - asteroid.r > w) asteroid.x = -asteroid.r;
+  else if (asteroid.x + asteroid.r < 0) asteroid.x = w + asteroid.r;
+  if (asteroid.y - asteroid.r > h) asteroid.y = -asteroid.r;
+  else if (asteroid.y + asteroid.r < 0) asteroid.y = h + asteroid.r;
 }
 
-function draw(entity) {
+function drawPlayer(player) {
   ctx2d.save();
-  ctx2d.translate(entity.x, entity.y);
-  ctx2d.rotate(entity.angle * DEG2RAD);
-  ctx2d.scale(entity.scale, entity.scale);
-  ctx2d.drawImage(entity.sprite, -entity.r, -entity.r);
+  ctx2d.translate(player.x, player.y);
+  ctx2d.rotate(player.angle * DEG2RAD);
+  ctx2d.lineWidth = 2;
+  ctx2d.strokeStyle = "white";
+  ctx2d.beginPath();
+  ctx2d.moveTo(player.h * 0.5, 0);
+  ctx2d.lineTo(player.h * -0.5, player.w * 0.5);
+  ctx2d.lineTo(player.h * -0.5, player.w * -0.5);
+  ctx2d.lineTo(player.h * 0.5, 0);
+
+  // ctx2d.roundRect(
+  //   player.w * -0.5,
+  //   player.h * -0.5,
+  //   player.w,
+  //   player.h,
+  //   player.w / 10
+  // );
+  // ctx2d.roundRect(
+  //   player.w * -1,
+  //   player.h * -0.5,
+  //   player.w * 2,
+  //   player.h / 4,
+  //   player.w / 10
+  // );
+  ctx2d.stroke();
   ctx2d.restore();
 }
 
-function loadImgAsset(name) {
-  return new Promise((resolve) => {
-    const img = new Image();
-    img.src = `./assets/${name}.png`;
-    img.onload = resolve;
-    assets[name] = img;
-  });
+function drawAsteroid(asteroid) {
+  ctx2d.save();
+  ctx2d.translate(asteroid.x, asteroid.y);
+  ctx2d.rotate(asteroid.angle * DEG2RAD);
+  ctx2d.strokeStyle = "white";
+  ctx2d.lineWidth = asteroid_levels - asteroid.level + 1;
+  ctx2d.beginPath();
+  ctx2d.roundRect(
+    asteroid.r * -0.5,
+    asteroid.r * -0.5,
+    asteroid.r,
+    asteroid.r,
+    asteroid.r / 10
+  );
+  ctx2d.stroke();
+  ctx2d.restore();
 }
