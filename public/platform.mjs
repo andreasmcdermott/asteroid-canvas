4983e809c2d88722a4a6b6700e62a2bad20657e6
Andreas McDermott <andreas.mcdermott@shortcut.com>
2023-06-13 11:05:20 -0700

    gif recording wip.

--- public/platform.mjs
+++ public/platform.mjs
@@ -1,13 +1,55 @@
+import { GIFEncoder, quantize, applyPalette } from "https://unpkg.com/gifenc";
+
 let lt;
 let ctx;
 let w, h;
 let gameState;
 let canvas;
+let chunks;
+let fps = 25;
+let seconds = 10;
+let recording = false;
+let gifSize = 500;
+
+const startRecorder = () => {
+  if (recording) return;
+  chunks = new Array(fps * seconds);
+  recording = true;
+};
+
+const stopRecorder = async (e) => {
+  if (!recording) return;
+  recording = false;
+  const gif = GIFEncoder();
+  for (let i = 0; i < chunks.length; ++i) {
+    let data = chunks[i];
+    if (data) {
+      const palette = quantize(data, 256);
+      const index = applyPalette(data, palette);
+      gif.writeFrame(index, gifSize, gifSize, { palette });
+    }
+  }
+  gif.finish();
+  const output = gif.bytes();
+  const img = document.createElement("img");
+  img.width = gifSize;
+  img.height = gifSize;
+  img.src = URL.createObjectURL(
+    new Blob([output.buffer], { type: "image/gif" })
+  );
+  document.getElementById("output").appendChild(img);
+};
 
 let _Game;
 function gameLoop(dt) {
   try {
     _Game.gameLoop(dt, gameState);
+    if (gameState.screen !== "gameOver") {
+      startRecorder();
+    }
+    if (gameState.screen === "gameOver") {
+      setTimeout(stopRecorder, 100);
+    }
   } catch (err) {
     console.error(err);
   }
@@ -27,7 +69,7 @@ export function init(_canvas) {
   h = window.innerHeight;
   canvas.setAttribute("width", w);
   canvas.setAttribute("height", h);
-  ctx = canvas.getContext("2d");
+  ctx = canvas.getContext("2d", { willReadFrequently: true });
   loadGame(() => {
     gameState = _Game.initGame(w, h, ctx);
     initEventListeners();
@@ -36,11 +78,26 @@ export function init(_canvas) {
   });
 }
 
+let frameCounter = 0;
 function nextFrame(t) {
   let dt = t - lt; // dt is ~16ms
   lt = t;
   canvas.classList.toggle("mouse_active", gameState.mouse_active);
   gameLoop(dt);
+  if (recording) {
+    if (frameCounter++ > 4) {
+      let left = Math.max(gameState.player.p.x - gifSize / 2, 0);
+      let top = Math.max(gameState.player.p.y - gifSize / 2, 0);
+      if (left + gifSize > gameState.win.w) left = gameState.win.w - gifSize;
+      if (top + gifSize > gameState.win.h) top = gameState.win.h - gifSize;
+
+      const { data } = gameState.ctx.getImageData(left, top, gifSize, gifSize);
+
+      if (chunks.length >= fps * seconds - 1) chunks.shift();
+      chunks.push(data);
+      frameCounter = 0;
+    }
+  } else frameCounter = 0;
   // TODO: Can we grab the canvas content and create a video/screenshot?
   gameState.lastInput = { ...gameState.input };
   requestAnimationFrame(nextFrame);
@@ -62,6 +119,7 @@ function initEventListeners() {
       h = window.innerHeight;
       canvas.setAttribute("width", w);
       canvas.setAttribute("height", h);
+      chunks = new Array(fps * seconds);
       gameState.win.set(w, h);
     },
     { passive: true }
